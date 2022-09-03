962f76f7571f8e94bf3fe221d6819d80684e2751
rosmis <rosmis123@gmail.com>
2022-09-02 19:22:13 -0500

    code refactor for tablegrid

--- src/components/TableGrid.vue
+++ src/components/TableGrid.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col gap-4 items-center justify-center">
-    <TableRow :key-array="wordObject[1]" :evaluations="evaluations[1]" />
-    <TableRow :key-array="wordObject[2]" :evaluations="evaluations[2]" />
-    <TableRow :key-array="wordObject[3]" :evaluations="evaluations[3]" />
-    <TableRow :key-array="wordObject[4]" :evaluations="evaluations[4]" />
-    <TableRow :key-array="wordObject[5]" :evaluations="evaluations[5]" />
-    <TableRow :key-array="wordObject[6]" :evaluations="evaluations[6]" />
+    <TableRow
+      v-for="(row, index) in wordObject"
+      :key="index"
+      :key-array="wordObject[index]"
+      :evaluations="evaluations[index]"
+    />
   </div>
 </template>
 
@@ -16,6 +16,10 @@ import { options } from "./axios_params.js";
 import TableRow from "./TableRow.vue";
 
 // window.localStorage.clear();
+
+const props = defineProps({
+  keyInputObject: Object,
+});
 
 onMounted(() => {
   if (localStorage.wordleState) {
@@ -42,10 +46,6 @@ onMounted(() => {
     .catch(function (error) {
       console.error(error);
     });
-});
-
-const props = defineProps({
-  keyInputObject: Object,
 });
 
 let wordObject = ref({
