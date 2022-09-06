24633150014ac285613410bf9a608784db29aba7
rosmis <rosmis123@gmail.com>
2022-09-06 12:05:47 -0500

    axios params removed from gitignore

---
+++ src/components/axios_params.js
@@ -0,0 +1,12 @@
+const options = {
+    method: "GET",
+    url: "https://random-words5.p.rapidapi.com/getRandom",
+    params: { wordLength: "5" },
+    headers: {
+      'X-RapidAPI-Key': '4b4e7879f2msh9885e594e0c931fp15e474jsn4d84868494af',
+      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+    },
+};
+
+export { options };
+
